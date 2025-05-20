2d73c9f852e42c197a9bd9d496951800703bea83
itc-s25008 <s25008@std.it-college.ac.jp>
2025-05-20 16:00:26 +0900

    P48 第3章 チャレンジ問題 終了

---
+++ Chapter03/challenge3-02.py
@@ -0,0 +1,6 @@
+x = int(input("変数を入力してください!"))
+if x < 10:
+    print("10未満")
+else:
+    print("10以上")
+
---
+++ Chapter03/challenge3-06.py
@@ -0,0 +1,6 @@
+age = int(input("年齢を入力してください!"))
+if age<18:
+    print("未成年後", 18 - age, "待て!")
+else:
+    print("成人済")
+
